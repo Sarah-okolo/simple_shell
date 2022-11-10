aa6027a7c1db40f5105b6be7b01b33eed2f8ebde
Sarah-okolo <okolosarah402@gmail.com>
2022-11-10 22:35:42 +0100

    handled prompt and a few command executions

---
+++ prompt.c
@@ -0,0 +1,31 @@
+#include "shell.h"
+
+/**
+ * prompt - creates a prompt to allow input of commands to be run in ths shell
+ *
+ * Return: the command string inputted by the user in the prompt
+ */
+
+char *prompt(void)
+{
+	char *buffer, *new_buffer;
+	size_t len;
+	
+//allocates memory for various the buffer strings
+	buffer = malloc(sizeof(char) * 1024);
+	new_buffer = malloc(sizeof(char) * 1024);
+
+//displays the prompt string in the form "[user]($)"
+	dprintf(STDOUT_FILENO, "[%s]($) ", getenv("LOGNAME"));
+
+
+// reads a line from the standard input until a newline is encountered and stores it into the buffer variable
+	getline(&buffer, &len, stdin);
+
+//strip the string of the '\n' character that is usually returned  from the getline function
+	new_buffer = strtok(buffer, "\n");
+
+	free(buffer);
+
+	return (new_buffer);
+}
---
+++ welcome_screen.c
@@ -0,0 +1,15 @@
+#include "shell.h"
+
+/**
+ * welcome_screen - prints out a basic starting screen on first execution of
+ * the shell in a new proccess.
+ */
+
+void welcome_screen(void){
+        printf("\n\t============================================\n");
+        printf("\t               Simple_Shell\n");
+        printf("\t--------------------------------------------\n");
+        printf("\t             A lightweight UNIX shell\n");
+        printf("\t============================================\n");
+        printf("\n\n");
+}
