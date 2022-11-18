cbe2e5924d6ef0761f07b4478944e4d682b8e340
Henzz <khenok53@gmail.com>
2022-11-18 14:54:00 -0800

    changed interactive exit status to atty return val and non interactive to two

--- Built-in.c
+++ Built-in.c
@@ -62,7 +62,7 @@ void exit_b(char *line)
 		_write_char('\n');
 		print_str("exiting simple shell\n", 1);
 		print_str("======================================\n\n", 1);
-		exit(0);
+		exit(isatty(STDIN_FILENO));
 	}
 	exit(2);
 }
